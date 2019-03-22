f0bb572c4453e59a5b581416042d7468f8c09607
zappyfish <liamkelly2015@gmail.com>
2019-03-22 16:48:42 -0500

    updates

--- applications/jetson_spi/main.cpp
+++ applications/jetson_spi/main.cpp
@@ -6,31 +6,37 @@
 #include "spi_bus.h"
 #include "nrf_handler.h"
 #include "rf_packet.h"
-#include "rpi_file_manager.h"
 
-void callback(rf_packet packet);
+void callback(rf_packet packet, void *args);
 
 int main() {
 
     rpi_file_manager *f_manager = new rpi_file_manager();
     data_logger::get_instance().start_flight_session(f_manager);
 
-    nrf_handler handler(nrf_handler::board_type::jetson, nrf_handler::mode::TX, 254, nullptr);
+    nrf_handler::rf_callback cb;
+
+    nrf_handler handler(nrf_handler::board_type::jetson, nrf_handler::mode::RX, 254, nullptr);
+
+    cb.callback = &callback;
 
     handler.verify_spi();
 
-    rf_packet test_packet(-5, 1);
-
-    handler.send_packet(test_packet);
-
-
+//    rf_packet test_packet(-5, 1);
+//
+//    handler.send_packet(test_packet);
+//
+//
     while (true) {
-        handler.resend_last_packet();
+        // handler.resend_last_packet();
+        handler.check_packets();
     }
+
+
 
 }
 
-void callback(rf_packet packet) {
+void callback(rf_packet packet, void *args) {
     std::cout << "callback on a packet\n";
     std::cout << packet.get_gps_x() << ", " << packet.get_gps_y() << std::endl;
 }
