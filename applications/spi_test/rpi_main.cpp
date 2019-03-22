679e008496e7df21cdcdccb78a367f907c4cbbb9
zappyfish <liamkelly2015@gmail.com>
2019-03-22 17:08:49 -0500

    updates

--- applications/spi_test/rpi_main.cpp
+++ applications/spi_test/rpi_main.cpp
@@ -18,7 +18,7 @@ int main() {
 
     nrf_handler::rf_callback cb;
 
-    nrf_handler handler(nrf_handler::board_type::rpi, nrf_handler::mode::TX, 25, &cb);
+    nrf_handler handler(nrf_handler::board_type::rpi, nrf_handler::mode::RX, 25, &cb);
 
     cb.callback = &callback;
 
