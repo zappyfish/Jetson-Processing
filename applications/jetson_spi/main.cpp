12c5b1ff195323344da365b8d01a397e76eb2b2b
zappyfish <liamkelly2015@gmail.com>
2019-04-02 11:49:27 -0500

    updates

--- applications/jetson_spi/main.cpp
+++ applications/jetson_spi/main.cpp
@@ -2,36 +2,48 @@
 // Created by Liam Kelly on 3/13/19.
 //
 
-#include "uav_gpio.h"
-#include "spi_bus.h"
-#include "nrf_handler.h"
-#include <thread>
-#include "rf_packet.h"
+//#include "uav_gpio.h"
+//#include "spi_bus.h"
+//#include "nrf_handler.h"
+//#include <thread>
+//#include "rf_packet.h"
+
+#include <RF24/RF24.h>
 
 void callback(rf_packet packet, void *args);
 
 int main() {
 
-    nrf_handler::rf_callback cb;
+//    nrf_handler::rf_callback cb;
+//
+//    nrf_handler handler(nrf_handler::board_type::jetson, nrf_handler::mode::TX, 254, nullptr);
+//
+//    cb.callback = &callback;
+//
+//    handler.verify_spi();
+//
+//    rf_packet test_packet(-5, 1);
+//
+//    handler.send_packet(test_packet);
+//
+//
+//    while (true) {
+//        handler.resend_last_packet();
+//
+//        // handler.check_packets();
+//    }
 
-    nrf_handler handler(nrf_handler::board_type::jetson, nrf_handler::mode::TX, 254, nullptr);
-
-    cb.callback = &callback;
-
-    handler.verify_spi();
-
-    rf_packet test_packet(-5, 1);
-
-    handler.send_packet(test_packet);
+// Setup for GPIO 15 CE and CE0 CSN with SPI Speed @ 8Mhz
+    RF24 radio(RPI_V2_GPIO_P1_15, RPI_V2_GPIO_P1_24, BCM2835_SPI_SPEED_8MHZ);
 
 
-    while (true) {
-        handler.resend_last_packet();
+    // Setup and configure rf radio
+    radio.begin();
 
-        // handler.check_packets();
-    }
-
-
+    // optionally, increase the delay between retries & # of retries
+    radio.setRetries(15,15);
+    // Dump the configuration of the rf unit for debugging
+    radio.printDetails();
 
 }
 
--- applications/spi_test/rpi_main.cpp
+++ applications/spi_test/rpi_main.cpp
@@ -6,41 +6,45 @@
 // Created by Liam Kelly on 3/13/19.
 //
 
-#include "uav_gpio.h"
-#include "spi_bus.h"
-#include "nrf_handler.h"
-#include "rf_packet.h"
+//#include "uav_gpio.h"
+//#include "spi_bus.h"
+//#include "nrf_handler.h"
+//#include "rf_packet.h"
+#include <RF24/RF24.h>
 
 void callback(rf_packet packet, void *args);
 
 int main() {
 
 
-    nrf_handler::rf_callback cb;
-
-    nrf_handler handler(nrf_handler::board_type::rpi, nrf_handler::mode::RX, 25, &cb);
-
-    cb.callback = &callback;
-
-
-    handler.verify_spi();
-
-    std::cout << "starting loop\n";
-
-//    rf_packet test_packet(-5, 1);
+//    nrf_handler::rf_callback cb;
 //
-//    handler.send_packet(test_packet);
+//    nrf_handler handler(nrf_handler::board_type::rpi, nrf_handler::mode::RX, 25, &cb);
+//
+//    cb.callback = &callback;
+//
+//
+//    handler.verify_spi();
+//
+//    std::cout << "starting loop\n";
+//
+////    rf_packet test_packet(-5, 1);
+////
+////    handler.send_packet(test_packet);
+//
+//    while (true) {
+//        handler.check_packets();
+//        std::this_thread::sleep_for(std::chrono::milliseconds(50)); // let it settle
+//
+//        // handler.resend_last_packet();
+//    }
+//
+//    handler.set_mode(nrf_handler::mode::RX);
+//
+//    handler.check_packets();
 
-    while (true) {
-        handler.check_packets();
-        std::this_thread::sleep_for(std::chrono::milliseconds(50)); // let it settle
-
-        // handler.resend_last_packet();
-    }
-
-    handler.set_mode(nrf_handler::mode::RX);
-
-    handler.check_packets();
+    RF24 radio(25,0);
+    
 
 }
 
